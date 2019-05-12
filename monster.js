f24e702bd6c4120af8a628332d5e92e010d19d0f
simon3000 <simon3000@163.com>
2019-05-12 13:25:15 +0800

    Update monster.js

--- monster.js
+++ monster.js
@@ -21,3 +21,20 @@ nodejieba.load({
   nodejieba.extract(day, 250).forEach(w => console.log(`${w.word};${Math.round(w.weight)}`))
   // nodejieba.extract(day, 100).forEach(w => console.log(w))
 })()
+
+
+// (async () => {
+//   let day = String(await fs.readFile('12235923/2019-5-11.txt'))
+//     .split('\n')
+//     .filter(w => !(w.includes('TIME') && w.includes('ONLINE') || w.includes('SPEAKERNUM')))
+//     .join('\n')
+//   let k = ''
+//   nodejieba.extract(day, 250).forEach(w => {
+//     for (let i = 0; i < Math.round(w.weight); i++) {
+//       k += `${w.word} `
+//     }
+//     // console.log(`${w.word};${Math.round(w.weight)}`)
+//   })
+//   console.log(k)
+//   // nodejieba.extract(day, 100).forEach(w => console.log(w))
+// })()
